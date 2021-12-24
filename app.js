70fa77d30fc725439af04ea975849ab20c6eca5b
Tikpeme <tete.ikpeme@gmail.com>
2021-12-24 12:33:39 -0500

    Starting draft. Using Weather messages

---
+++ app.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-console */
+/* Code planning
+ Code will output a single random message to the console everytime the function runs ( Random but somehat grammtically coherent)
+ There i need to plan what combination of phrases of word can always go together
+*/
+
+const time = ["Today", "Tomorrow", "Tonight", "This week"];
+
+const adjective = ["Slighly", "Extremely", "very"];
+
+console.log(time);
