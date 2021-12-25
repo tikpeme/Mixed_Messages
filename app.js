0ffe4dda33d70fe6ed85173544e59f9082b47765
Tikpeme <tete.ikpeme@gmail.com>
2021-12-24 20:55:34 -0500

    First draft

--- app.js
+++ app.js
@@ -6,6 +6,67 @@
 
 const time = ["Today", "Tomorrow", "Tonight", "This week"];
 
-const adjective = ["Slighly", "Extremely", "very"];
+const adjective = [
+  "slightly",
+  "extremely",
+  "very",
+  " ",
+  "partially",
+  "sporadically",
+  "a little",
+];
 
-console.log(time);
+const weather = [
+  "sunny",
+  "chilly",
+  "windy",
+  "frosty",
+  "snowy",
+  "cloudy",
+  "rainy",
+  "haily",
+  "hot",
+  "humid",
+];
+
+const expressionsForCold = [
+  "you should consider wearing a jacket.",
+  "you should don a jacket.",
+  "you may want to probably layer up.",
+];
+
+const expressionsForWarm = [
+  "Wear something light.",
+  "Consider swapping out the sweater for a t-shirt.",
+  "Think of putting on bright reflective colors.",
+];
+
+const makeSentence = () => {
+  const randomTime = time[Math.floor(Math.random() * time.length)];
+  const randomAdjective =
+    adjective[Math.floor(Math.random() * adjective.length)];
+  const randomWeather = weather[Math.floor(Math.random() * weather.length)];
+  let randomExpression;
+  // Selection of sentence structure based on weather
+  if (
+    randomWeather === "chilly" ||
+    randomWeather === "frosty" ||
+    randomWeather === "snowy" ||
+    randomWeather === "rainy"
+  ) {
+    randomExpression =
+      expressionsForCold[Math.round(Math.random() * expressionsForCold.length)];
+  } else if (
+    randomWeather === "sunny" ||
+    randomWeather === "swealtering hot" ||
+    randomWeather === "humid" ||
+    randomWeather === "cloudy"
+  ) {
+    randomExpression =
+      expressionsForWarm[Math.round(Math.random() * expressionsForWarm.length)];
+  } else randomExpression = "";
+  console.log(
+    `${randomTime}'s weather will be ${randomAdjective} ${randomWeather}. ${randomExpression}`
+  );
+};
+makeSentence();
